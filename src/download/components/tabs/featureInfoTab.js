cacd372295c532d69129e4e0aeeedd221a8a91ea
johannes <johannes.nyman@csc.fi>
2021-02-18 11:18:50 +0200

    added accessibility statement

--- src/download/components/tabs/featureInfoTab.js
+++ src/download/components/tabs/featureInfoTab.js
@@ -16,7 +16,6 @@ function init(event) {
       .getFeatureInfoUrl(event.coordinate, resolution, 'EPSG:3857', {
         INFO_FORMAT: 'text/plain',
       })
-      .replace('/gwc/service', '')
     if (url) {
       const iframe =
         '<iframe id="feature-info-iframe" seamless src="' + url + '"></iframe>'
