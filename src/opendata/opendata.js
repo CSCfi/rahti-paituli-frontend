c123d7823e5beca85f59b6e5e69400298b8fc438
Kylli Ek <kylli.ek@csc.fi>
2021-10-28 09:52:38 +0300

    remove main.css (double)

--- src/opendata/opendata.js
+++ src/opendata/opendata.js
@@ -6,7 +6,6 @@ import { toggleTabActivation } from '../shared/header'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery-ui-bundle/jquery-ui.css'
-import '../../css/main.css'
 
 $(function () {
   $('#header').load('header.html')
